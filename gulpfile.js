b7df6446ef5123d0133da3c256a7533c5380acb2
Alex.Silver <silwar40k@gmail.com>
2019-01-19 02:53:59 +0300

    Настроил gulp, поднял browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,65 @@
 
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const debug = require('gulp-debug');
+const sourcemaps = require('gulp-sourcemaps');
+const del = require('del');
+const path = require('path');
+const newer = require('gulp-newer');
+const imagemin = require('gulp-imagemin');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
 
 sass.compiler = require('node-sass');
 
-gulp.task('sass', function(){
-    return gulp.src('source/scss/**/*.scss')
-        .pipe(sass.sync().on('error', sass.logError))
-        .pipe(gulp.dest('dest/css'));
+const srcDir = 'source';
+const destDir = 'dest';
+
+gulp.task('html', function() {
+    return gulp.src(srcDir + '/index.html', {since: gulp.lastRun('html')})
+        .pipe(newer(destDir))
+        .pipe(gulp.dest(destDir))
 });
 
-gulp.task('watch', function () {
-    gulp.watch('source/scss/**/*.scss', gulp.series('sass'));
+gulp.task('styles', function() {
+    return gulp.src(srcDir + '/scss/app.scss', {since: gulp.lastRun('styles')})
+        .pipe(gulp.dest(destDir + '/css'))
+        .pipe(sourcemaps.init())
+        .pipe(autoprefixer())
+        .pipe(sass.sync({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(destDir + '/css'));
 });
+
+gulp.task('js', function() {
+    return gulp.src(srcDir + '/js/app.js', {since: gulp.lastRun('js')})
+        .pipe(newer(destDir + '/js'))
+        .pipe(gulp.dest(destDir + '/js'))
+});
+
+gulp.task('img', function() {
+    return gulp.src(srcDir + '/img/**/*.*', {since: gulp.lastRun('img')})
+        .pipe(newer(destDir + '/img'))
+        .pipe(imagemin())
+        .pipe(gulp.dest(destDir + '/img'))
+});
+
+gulp.task('build', gulp.parallel('html', 'styles', 'js', 'img'));
+
+gulp.task('watch', function () {
+    gulp.watch(srcDir + '/**/*.*', gulp.series('html', 'styles', 'js', 'img')).on('unlink', function (filepath) {
+        const filePathFromSrc = path.relative(path.resolve(srcDir), filepath);
+        const destFilePath = path.resolve(destDir, filePathFromSrc);
+        del.sync(destFilePath);
+    });
+});
+
+gulp.task('serv', function () {
+    browserSync.init({
+        server: 'dest'
+    });
+
+    browserSync.watch('dist/**/*.*').on('change', browserSync.reload);
+});
+
+gulp.task('dev', gulp.series('build', gulp.parallel('watch', 'serv')));
